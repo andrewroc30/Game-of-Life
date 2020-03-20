0b096e207ad707b3deff5e72409bb9206a195937
Andrew Rocchi <andrewroc30@gmail.com>
2020-03-20 17:10:39 -0400

    Intitial Commit

---
+++ game.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
